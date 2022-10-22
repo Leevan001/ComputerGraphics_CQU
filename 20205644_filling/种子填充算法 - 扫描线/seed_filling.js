9822ed534094ba05a3cc5b040c91fabaf6e0be13
Leevan001 <1249824014@qq.com>
2022-10-22 22:41:17 +0800

    更新了扫描线种子填充算法和中点分割裁剪算法

---
+++ 20205644_filling/种子填充算法 - 扫描线/seed_filling.js
@@ -0,0 +1,141 @@
+//console.log('Hello World');
+var zuobiao=[];
+var canvas = document.getElementById('seed_filling_area');
+var ctx = canvas.getContext('2d');
+var width = canvas.width;
+var height = canvas.height;
+var mystack=[];
+var flag=true;
+var imageData;
+// ctx.strokeStyle = 'green';
+// ctx.moveTo(0,0);//坐标起点
+// ctx.lineTo(1,1);//终点,或者理解为下一个点
+// ctx.stroke();//进行绘制
+// imageData=ctx.getImageData(0, 0, width, height).data;
+// console.log(imageData);
+
+    //获取鼠标点击的坐标
+function getPoint(event) {
+    // canvas = document.getElementById('seed_filling_area');
+    // ctx = canvas.getContext('2d');
+    tmp_x = event.offsetX;
+    tmp_y = event.offsetY;
+    zuobiao.push([tmp_x,tmp_y]);
+    //console.log(flag);
+
+    // ctx.beginPath();//开始绘制
+    // ctx.lineWidth = 1;
+    // ctx.strokeStyle = 'black';
+    // ctx.moveTo(tmp_x, tmp_y);//坐标起点
+    // ctx.lineTo(tmp_x+1, tmp_y+1);//终点,或者理解为下一个点
+    // ctx.stroke();//进行绘制
+
+    if (zuobiao.length>1&&flag){
+        drawline();      
+    }   
+}
+function drawline(){
+    //绘制线段
+    ctx.beginPath();//开始绘制
+    ctx.lineWidth = 6;
+    ctx.strokeStyle = 'blue';
+    start_x=zuobiao[zuobiao.length-2][0],start_y=zuobiao[zuobiao.length-2][1];
+    end_x=zuobiao[zuobiao.length-1][0],end_y=zuobiao[zuobiao.length-1][1];
+    ctx.moveTo(start_x,start_y);//坐标起点
+    //线的端点是圆，还可以是butt(正方)，square(正方，但是追加一段长为线段厚度一半的矩形区域)
+    ctx.lineCap = 'square';
+    ctx.lineWidth =1;
+    ctx.lineTo(end_x, end_y);
+    ctx.stroke();
+}
+
+function pix_close(){//首尾闭合
+    ctx.moveTo(zuobiao[zuobiao.length-1][0],zuobiao[zuobiao.length-1][1]);//首尾闭合
+    ctx.lineCap = 'square';
+    ctx.lineWidth =1;
+    ctx.lineTo(zuobiao[0][0], zuobiao[0][1]);
+    ctx.stroke();
+    zuobiao=[];
+    flag=false;
+    //zuobiao.push([zuobiao[0][0],zuobiao[0][1]]);
+    // console.log('height',height);
+    // console.log('width',width);
+    // console.log(ctx.getImageData(0, 0, width, height));
+}
+
+function filling_start() {//进行填充
+    var Xl, Xr,XR,XL;
+    //console.log('进行填充');
+   // console.log(zuobiao);
+    imageData=ctx.getImageData(0, 0, width, height);//获取画布已有信息
+    //console.log(imageData);
+    mystack.push(zuobiao[0]);
+    //console.log('栈的长度',mystack.length);
+    while(mystack.length!=0){//扫描线
+        [tmp_x, tmp_y] = mystack.pop();
+        tmp_xl = tmp_x;
+        tmp_xr = tmp_x;
+       // console.log('x',tmp_x,'y',tmp_y);
+        //console.log('栈的长度',mystack.length);
+        while(if_colored(tmp_xr+1,tmp_y)==false){//说明没有着色
+            draw_dot(tmp_xr + 1, tmp_y);
+            tmp_xr += 1;
+            Xr = tmp_xr;
+            XR = tmp_xr;
+            //console.log('栈的长度',mystack.length);
+        }
+        while(if_colored(tmp_xl,tmp_y)==false){//说明没有着色
+            draw_dot(tmp_xl , tmp_y);
+            tmp_xl -= 1;
+            Xl = tmp_xl;
+            XL = tmp_xl;
+           /// console.log('栈的长度',mystack.length);
+        }
+        while (XR >= XL) {
+            if (if_colored(XR, tmp_y + 1) == false) {
+                mystack.push([XR, tmp_y + 1]);
+                break;
+            }
+            XR--;
+            //console.log("下方",XR,XL);
+        }
+        while (Xr >= Xl) {
+            if (if_colored(Xr, tmp_y - 1) == false) {
+                mystack.push([Xr, tmp_y - 1]);
+                break;
+            }
+            Xr--;
+            //console.log("上方", Xr, Xl);
+        }
+        ctx.putImageData(imageData, 0, 0);//显示在画布上
+    }
+}
+
+//判断是否已经着色
+function if_colored(x, y) {//设置中点画圆算法画布上的像素点颜色
+   // console.log('调用函数if_colored',x,y);
+    var index = getStartIndex(x, y);
+   // console.log(imageData.data[index+3]);
+   // console.log('index',index);
+    if(imageData.data[index+3]!=0){
+        //console.log('返回',true);
+        return true;
+    }
+    else{
+        //console.log('返回',false);
+        return false;
+    }
+}
+
+function getStartIndex(x, y) {//rgba，获取在(x,y)在一维数组中的起始位置
+
+    return y * width * 4 + x * 4;
+}
+
+function draw_dot(x, y) {//设置中点画圆算法画布上的像素点颜色
+    //console.log('调用函数draw_dot');
+    var index = getStartIndex(x, y);
+    imageData.data[index + 3] = 255;
+    //ctx.putImageData(imageData, 0, 0);//显示在画布上
+}
+//本打算一个点一个点显示的，但是被浏览器优化了，只能一起显示
--- line_Cut/Midpoint Subdivision Line Clipping/Midpoint SubdivisionLineClipping.js
+++ line_Cut/Midpoint Subdivision Line Clipping/Midpoint SubdivisionLineClipping.js
@@ -1,3 +1,4 @@
+//console.log('Hello World!');
 var zuobiao=[],rect_flag=false,rect=[];
 //console.log('Hello World!');
 function getPoint(event) {
@@ -65,6 +66,17 @@ function drawline2(x1,y1,x2,y2){//将在区域内的直线变成黑色
 function create_rec(){
     rect_flag=true; 
 }
+function exchange(x1,x2){
+    return [x2,x1];
+}
+function encode(line_x1,line_y1,xmin,xmax,ymin,ymax){
+    var c1=0;
+    if(line_x1<xmin)c1+=1;//convas画布y轴向下
+    if(line_x1>xmax)c1+=2;
+    if(line_y1<ymin)c1+=4;
+    if(line_y1>ymax)c1+=8;
+    return c1;
+}
 function line_cut(){
     rect_flag=false;
     [x1,y1]=rect[0];//矩形对角坐标
@@ -80,55 +92,43 @@ function line_cut(){
         while(1){
             //确定点的区域
             var c1=0,c2=0;
-            if(line_x1<xmin)c1+=1;//convas画布y轴向下
-            if(line_x1>xmax)c1+=2;
-            if(line_y1<ymin)c1+=4;
-            if(line_y1>ymax)c1+=8;
-            if(line_x2<xmin)c2+=1;
-            if(line_x2>xmax)c2+=2;
-            if(line_y2<ymin)c2+=4;
-            if(line_y2>ymax)c2+=8;
-            console.log('c1',c1.toString(2),'c2',c2.toString(2));
+            c1=encode(line_x1,line_y1,xmin,xmax,ymin,ymax);
+            c2=encode(line_x2,line_y2,xmin,xmax,ymin,ymax);
+            //console.log('c1',c1.toString(2),'c2',c2.toString(2));
             if((c1|c2)==0){//都在里面
                 drawline2(line_x1,line_y1,line_x2,line_y2);
-                break;
-            }else if((c1&c2)!=0){//都在外面且同区域
+                console.log('cut后画线坐标：',line_x1,'|',line_y1,'|',line_x2,'|',line_y2);
                 break;
             }else{
-                if(c1==0){
-                    tmpx=line_x1;
-                    line_x1=line_x2;
-                    line_x2=tmpx;
-                    tmpy=line_y1;
-                    line_y1=line_y2;
-                    line_y2=tmpy;
-                    tmpc=c1;
-                    c1=c2;
-                    c2=tmpc;
+                if(c1==0){//（1）令窗外端点为P1，如果窗外点不是P1，则P1和P2交换端点；
+                    [line_x1,line_x2]=exchange(line_x1,line_x2);
+                    [c1,c2]=exchange(c1,c2);
+                    [line_y1,line_y2]=exchange(line_y1,line_y2);
                 }
-                if(c1&1){
-                
-                    line_y1 = Math.round(line_y1 + ((xmin-line_x1) * (line_y1-line_y2)/(line_x1-line_x2)));
-                    line_x1=xmin;
+                //（2）保留窗内端点P2到暂存器里
+                tmp_linex2=line_x2,tmp_liney2=line_y2;
+                while(Math.abs(line_x1-line_x2)>1||Math.abs(line_y1-line_y2)>1){
+                    //（3）对P1编码为C1
+                    c1=encode(line_x1,line_y1,xmin,xmax,ymin,ymax);
+                    console.log('c1',c1.toString(2),'c2',c2.toString(2));
+                    //（4）用中点公式求出中点 ，并编码得C
+                    c_x=Math.round((line_x1+line_x2)/2);
+                    c_y=Math.round((line_y1+line_y2)/2);
+                    console.log("c_x",c_x,"c_y",c_y);
+                    c=encode(c_x,c_y,xmin,xmax,ymin,ymax);
+                    //（5）按照中点算法的求交规则：
+                    //若P1和P同侧，移动P1点；if((C1&C)!=0)　P1=P;否则，移动P2点。else P2=P;
+                    if((c1&c)!=0){
+                        line_x1=c_x;
+                        line_y1=c_y;
+                    }else{
+                        line_x2=c_x;
+                        line_y2=c_y;
+                    }//（6）流程转至（3），直到P1和P2相差一个单位时：令交点为P2，取出暂存器的端点赋给P1，然后转向流程1
                 }
-                if(c1&2){
-                    line_y1 = Math.round(line_y1 + ((xmax-line_x1) * (line_y1-line_y2)/(line_x1-line_x2)));
-                    line_x1 = xmax;
-                }
-                if(c1&4){
-                    line_x1 = Math.round(line_x1 + ((ymin-line_y1) * (line_x1-line_x2)/(line_y1-line_y2)));
-                    line_y1 = ymin;
-                }
-                if(c1&8){
-                    line_x1 = Math.round(line_x1 + ((ymax-line_y1) * (line_x1-line_x2)/(line_y1-line_y2)));
-                    line_y1 = ymax;
-                }
+                line_x1=tmp_linex2,line_y1=tmp_liney2;
+                console.log('执行完一次循环','p1:',line_x1,'.',line_y1,'p2:',line_x2,'.',line_y2);
             }
         }
-    }
-
-    
-
-    
-    
+    } 
 }
